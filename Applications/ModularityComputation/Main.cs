63049007f9c04590f01fc3b7d28bb65317fc79ff
Ingo Scholtes <ingos@gmxpro.de>
2012-01-10 20:32:10 +0100

    Increased precision

--- Applications/ModularityComputation/Main.cs
+++ Applications/ModularityComputation/Main.cs
@@ -11,21 +11,24 @@ namespace ModularityComputation
 		{
 			if(args.Length<1)
 			{
-				Console.WriteLine("Usage: ModularityComputation [network_file] [directed=false] [count_eii=true] [visualize=false]");
+				Console.WriteLine("Usage: ModularityComputation [network_file] [directed=false] [count_eii=false] [visualize=false] [reduce_to_LCC=false]");
 				return;
 			}
 			bool directed = false;
 			if(args.Length>=2)
 				directed = Boolean.Parse(args[1]);
-			ClusterNetwork n = ClusterNetwork.LoadNetwork(args[0], directed);			
+			ClusterNetwork n = ClusterNetwork.LoadNetwork(args[0], directed);					
+			
+			if(args.Length>4 && Boolean.Parse(args[4]) == true)
+				n.ReduceToLargestConnectedComponent();
 			
 			if(args.Length>=3 && Boolean.Parse(args[2]) == true)
-				Console.WriteLine("Q = {0:0.000}", n.NewmanModularity);
+				Console.WriteLine("Q = {0:0.000000}", n.NewmanModularity);
 			else
-				Console.WriteLine("Q = {0:0.000}", n.NewmanModularityWithoutEii);
+				Console.WriteLine("Q = {0:0.000000}", n.NewmanModularityWithoutEii);
 			
 			if(args.Length>3 && Boolean.Parse(args[3]) == true)
-			{			
+			{
 				NETGen.Visualization.NetworkVisualizer.Start(n, new NETGen.Layouts.FruchtermanReingold.FruchtermanReingoldLayout(10), new NETGen.Visualization.NetworkColorizer(), 800,600);			
 				NETGen.Visualization.NetworkVisualizer.Layout.DoLayout();
 			}
